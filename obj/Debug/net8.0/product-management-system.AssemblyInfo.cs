4e4a8c1b1c41021424d047feea8749720a962e91
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-12-04 00:12:13 -0300

    build: adicionar pacotes do Entity Framework Core (EF Core)
    Este commit instala os pacotes NuGet essenciais para configurar a persistência de dados usando o Entity Framework Core 8.0.0 com o SQL Server.
    
    Pacotes adicionados:
    - `Microsoft.EntityFrameworkCore.Design` (para comandos de migração).
    - `Microsoft.EntityFrameworkCore` (o core do EF).
    - `Microsoft.EntityFrameworkCore.Tools` (ferramentas de linha de comando).
    - `Microsoft.EntityFrameworkCore.SqlServer` (provedor para SQL Server).

--- obj/Debug/net8.0/product-management-system.AssemblyInfo.cs
+++ obj/Debug/net8.0/product-management-system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("product-management-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c7f3cab02b3cdb26fd404aefcb172940828e66")]
 [assembly: System.Reflection.AssemblyProductAttribute("product-management-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("product-management-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
